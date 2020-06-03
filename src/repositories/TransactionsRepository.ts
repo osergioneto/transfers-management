7a7e642fefc23150424a2136dac5e336d4cf7e66
Sérgio Neto <sergio.deusdedith@cubos.io>
2020-06-02 22:16:36 -0300

    refactor: remove unnecessary code on TransactionRepository

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -21,15 +21,10 @@ interface Transactions {
 
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
-  private transactions: Transaction[];
-
-  constructor() {
-    super();
-    this.transactions = [];
-  }
-
   public async getBalance(): Promise<Balance> {
-    const balance: Balance = this.transactions.reduce(
+    const transactions = await this.find();
+
+    const balance: Balance = transactions.reduce(
       (accumulator, currentValue) => {
         if (currentValue.type === 'income') {
           accumulator.income += currentValue.value;
@@ -45,14 +40,6 @@ class TransactionsRepository extends Repository<Transaction> {
     );
 
     return balance;
-  }
-
-  public create({ title, value, type }: TransactionDTO): Transaction {
-    const transaction = new Transaction({ title, value, type });
-
-    this.transactions.push(transaction);
-
-    return transaction;
   }
 }
 
