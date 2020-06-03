66f3dbefb3cb1aba80ebee79b868ead9e2f2774a
Sérgio Neto <sergio.deusdedith@cubos.io>
2020-06-02 22:12:12 -0300

    feat: add transaction repository and create transaction service

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -8,10 +8,51 @@ interface Balance {
   total: number;
 }
 
+interface TransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
+interface Transactions {
+  transactions: Transaction[];
+  balance: Balance;
+}
+
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
+  private transactions: Transaction[];
+
+  constructor() {
+    super();
+    this.transactions = [];
+  }
+
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const balance: Balance = this.transactions.reduce(
+      (accumulator, currentValue) => {
+        if (currentValue.type === 'income') {
+          accumulator.income += currentValue.value;
+          accumulator.total += currentValue.value;
+        } else {
+          accumulator.outcome += currentValue.value;
+          accumulator.total -= currentValue.value;
+        }
+
+        return accumulator;
+      },
+      { income: 0, outcome: 0, total: 0 },
+    );
+
+    return balance;
+  }
+
+  public create({ title, value, type }: TransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,35 @@
-// import AppError from '../errors/AppError';
-
+import AppError from '../errors/AppError';
 import Transaction from '../models/Transaction';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 
 class CreateTransactionService {
+  private transactionsRepository: TransactionsRepository;
+
+  constructor(transactionsRepository: TransactionsRepository) {
+    this.transactionsRepository = transactionsRepository;
+  }
+
+  private hasFund(value: number): void {
+    const { total } = this.transactionsRepository.getBalance();
+
+    if (value > total) {
+      throw new AppError("You can't create a outcome without a valid balance.");
+    }
+  }
+
   public async execute(): Promise<Transaction> {
-    // TODO
+    if (type === 'outcome') {
+      this.hasFund(value);
+    }
+
+    return this.transactionsRepository.create({ title, value, type });
   }
 }
 
